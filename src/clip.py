2d38ea55cc4992cc9a496004d179353154c91c40
anplaceb <anplaceb@gmail.com>
2017-06-02 15:20:42 +0200

    This is a test for Andrea

---
+++ src/clip.py
@@ -0,0 +1,35 @@
+import rasterio
+from dateutil.parser import parse
+import matplotlib.pyplot as plt
+import numpy as np
+
+#Test
+im1 = rasterio.open('clip1.tif') # imports image
+im1b1 = im1.read(1) # reads band 1
+date1 = im1.tags()['Acquisition_DateTime'] # reads date of the image
+im2 = rasterio.open('clip2.tif')
+im2b1 = im2.read(1)
+date2 = im2.tags() ['Acquisition_DateTime']
+print(type(im1.tags()['Acquisition_DateTime'])) # gives class of date
+
+date1 = parse(date1) # converts class string to datetime
+date2 = parse(date2)
+print(type(date1))
+
+dict = {'2015/5/10': im1b1, '2015/5/15' : im2b1}
+
+im1b1 = im1b1[:,:,np.newaxis] # adding a new dimension
+im2b1 = im2b1[:,:,np.newaxis]
+
+array3d = np.concatenate((im1b1, im2b1), axis = 2)  # concatenates the arrays by their 3rd dimension
+
+values = array3d[0,0,:] # extracts from all images the pixel values in the position row = 1, column = 1
+
+plt.plot(tuple([date1,date2]), ([array3d[0,0,:]]),'o')
+
+x1 = im1b1[0, 0] # subset first pixel of the image 1
+x2 = im2b1[0, 0]# subset first pixel of the image 2
+plt.plot([date1, date2], [x1, x2])  # plot time series
+
+
+[date1,date2]
