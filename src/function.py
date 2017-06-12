70dc36c4dc2833f3239127247513b8601b437b3d
anplaceb <anplaceb@gmail.com>
2017-06-12 20:07:52 +0200

    Function to read the file, the band and update the dictionary.

---
+++ src/function.py
@@ -0,0 +1,16 @@
+import rasterio
+def import_image(file, band, dict):
+    # "Reads the image file, takes the values of the selected band
+    # and of the acquisition time and updates a dictionary with them"
+    im = rasterio.open(file)
+    imb = im.read(band)
+    date = im.tags()['Acquisition_DateTime']
+    dict.update({date: imb})
+    print("Dictionary updated")
+    return 1
+
+tmp = import_image('clip1.tif',1,dict)
+
+
+
+
