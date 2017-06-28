1bf21f85347dcbd07d68f3df490073b7800839ca
anplaceb <anplaceb@gmail.com>
2017-06-28 20:05:09 +0200

    Not so useful function, still trying

--- timeseries.py
+++ timeseries.py
@@ -1,18 +1,14 @@
-import matplotlib.pyplot as plt
-import numpy as np
+def extract_timeseries(dic, row, column):
+    print([value[row, column] for value in dic.values()])
 
-#def
-#    [item[i,j] for item in dictionary.values()]
+extract_timeseries(dictionary, 1,2)
+# next step loop
+# associate with its key for the x axis of the plot
 
-dictionary["2015-05-15"][0,0]
-dictionary["2015-05-10"][0,0]
-
-x = [item[0,0] for item in dictionary.values()]
-
-d = {'numeros': ['1', '2', '3', '4'], 'letras': ['a', 'b', 'c', 'd']}
 
 for item in dictionary.values():
     print(item[0,0])
+
 
 plt.plot([x], 'o')
 
@@ -22,7 +18,7 @@ y = np.matrix("7 8 9; 10 11 12")
 
 d = {"a" : x, "b": y}
 
- [value[0] for value in d.values()]
+[value[0,1] for value in d.values()]
 
 for x in range(0,2):
     for y in range(0,3):
