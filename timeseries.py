11f2402eae2a4f7b21bfd9a5bb949eb69f554b7f
anplaceb <anplaceb@gmail.com>
2017-06-29 20:57:20 +0200

    Evolution of the function to extract the time series...

--- timeseries.py
+++ timeseries.py
@@ -1,25 +1,72 @@
+
 def extract_timeseries(dic, row, column):
+    # function not so useful because you have to give manually
+    # the number of row and column from which you want the timeseries
     print([value[row, column] for value in dic.values()])
 
-extract_timeseries(dictionary, 1,2)
-# next step loop
+
+def extract_timeseries(dic):
+    # a bit more useful, because the number row and column is integrated,
+    # but only specific for this picture size...
+    for x in range(0,528):
+        for y in range(0,720):
+            print([value[x, y] for value in dic.values()])
+
+
+def extract_timeseries(dic):
+    # prints lists of pairs of values with the same position in a dictionary
+    # that contains matrix
+    row = next(len(i) for i in dic.values()) + 1
+    col = next(i.shape[1] for i in dic.values()) + 1
+    for x in range(0, row):
+        for y in range(0,col):
+            print([value[x, y] for value in dic.values()])
+
+def extract_timeseries(dic):
+    # the same as the last one, but also prints class
+    # I tried to integrate here the function get_keys,
+    # but function ignores this part
+    row = next(len(i) for i in dic.values()) + 1
+    col = next(i.shape[1] for i in dic.values()) + 1
+    for x in range(0, row):
+        for y in range(0, col):
+            print([value[x, y] for value in dic.values()])
+            print(type([value[x, y] for value in dic.values()]))
+        for i in range(0, row*col+1):
+            print(dic.keys())
+
+def get_keys(dic):
+    # prints the keys of the dictionary (in this case the date info)
+    # for so many times as values in the matrix (in this case pixels)
+    row = next(len(i) for i in dic.values()) + 1
+    col = next(i.shape[1] for i in dic.values()) + 1
+    for i in range(0, row * col + 1):
+        print(dic.keys())
+
+# zip(get_keys(dictionary), extract_timeseries(dictionary) )
+
+#########################################################
+# next step
 # associate with its key for the x axis of the plot
+#########################################################
 
-
-for item in dictionary.values():
-    print(item[0,0])
-
-
-plt.plot([x], 'o')
-
-
+#########################################################
+# nice easy dictionary for inspiration
 x = np.matrix("1 2 3; 4 5 6")
 y = np.matrix("7 8 9; 10 11 12")
 
 d = {"a" : x, "b": y}
 
-[value[0,1] for value in d.values()]
+def extract_timeseries(dic):
+    for x in range(0,2):
+        for y in range(0,3):
+            print([value[x, y] for value in d.values()])
 
-for x in range(0,2):
-    for y in range(0,3):
-        print(x,y)
+def get_keys(dic):
+    for i in range(0,7):
+        print(d.keys())
+
+zip(extract_timeseries(d), get_keys(d))
+
+
+
