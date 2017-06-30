608d415ef7fdb0db3135c75662870d9d20a2f470
anplaceb <anplaceb@gmail.com>
2017-06-30 16:54:20 +0200

    ich hab main "aufgeräumt"

--- src/main.py
+++ src/main.py
@@ -1,41 +1,30 @@
 import numpy as np
-from timeseries import extract_timeseries
+from src.import_image import import_image
+
+
+
+
+# 1. Test: import_image function
+
+dictionary = {}
+image_file_1 = 'data/clip1.tif'
+image_file_2 = 'data/clip2.tif'
+import_image(image_file_1, 1, dictionary)
+import_image(image_file_2, 1, dictionary)
+
+
+# 2. Test: easy dictionary
 x = np.matrix("1 2 3; 4 5 6")
 y = np.matrix("7 8 9; 10 11 12")
 
 d = {"a" : x, "b": y}
 
-
-def extract_timeseries(dic):
+def timeseries(dic):
     for x in range(0,2):
         for y in range(0,3):
             print([value[x, y] for value in dic.values()])
 
-length_dict = {a: len(value) for a, value in d.items()}
-
-np.empty(2,3,2)
-
  [value[0,0] for value in d.values()]
 
-for x in range(0,2):
-    for y in range(0,3):
-        print(x,y)
-
-for x in range(0,2):
-    for y in range(0,3):
-        print([value[x, y] for value in d.values()])
 
 
-values = list(zip(*d.values())) # gives [([1, 2, 3], [7, 8, 9]), ([4, 5, 6], [10, 11, 12])]
-pairs = []
-for value in values:
-    pairs.extend(list(zip(*value))) #adds (1, 7), (2, 8), ... to pairs list
-
-for pair in pairs:
-    print(pair)
-
-def currency_converter(rate, euros):
-    dollars=euros*rate
-    return dollars
-
-x = currency_converter(100, 1000)
