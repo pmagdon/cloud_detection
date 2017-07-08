5a908f3ec703dd1757922a893aa0cb66e20be883
anplaceb <anplaceb@gmail.com>
2017-07-08 20:37:47 +0200

    for loop to extract time series for all the pixels in the image korrektur range (neither +1 nor -1 was needed, just the length value)

--- src/main.py
+++ src/main.py
@@ -1,8 +1,16 @@
 import numpy as np
 from src.import_image import import_image
 
+# 1. Test: for loop to extract time series for all pixel values and put it to a list
 
+timeseries_all = []
+for x in range(0, next(len(i) for i in dictionary.values())):
+    for y in range(0, next(i.shape[1] for i in dictionary.values())):
+        timeseries_all.append(extract_timeseries(dictionary, x, y))
 
+# and now put the values on the lists in dataframes
+for x in range(0,len(timeseries_all)):
+    pd.DataFrame(timeseries_all[x])
 
 
 # 2. Test: easy dictionary
@@ -14,8 +22,9 @@ d = {"a" : x, "b": y}
 
 def timeseries(dic):
     for x in range(0,2):
-        for y in range(0,3):
+        for y in range(0,1):
             print([value[x, y] for value in dic.values()])
+
 
  [value[0,0] for value in d.values()]
 
@@ -25,16 +34,16 @@ def timeseries(dic):
 def extract_timeseries(dic):
     # a bit more useful, because the number row and column is integrated,
     # but only specific for this picture size...
-    for x in range(0,528):
-        for y in range(0,720):
+    for x in range(0,527):
+        for y in range(0,719):
             print([value[x, y] for value in dic.values()])
 
 
 def extract_timeseries(dic):
     # prints lists of pairs of values with the same position in a dictionary
     # that contains matrix. also prints class and dictionary keys
-    row = next(len(i) for i in dic.values()) - 1
-    col = next(i.shape[1] for i in dic.values()) - 1
+    row = next(len(i) for i in dic.values())
+    col = next(i.shape[1] for i in dic.values())
     for x in range(0, row):
         for y in range(0,col):
             print([value[x, y] for value in dic.values()])
@@ -44,8 +53,8 @@ def extract_timeseries(dic):
 def get_keys(dic):
     # prints the keys of the dictionary (in this case the date info)
     # for so many times as values in the matrix (in this case pixels)
-    row = next(len(i) for i in dic.values()) + 1
-    col = next(i.shape[1] for i in dic.values()) + 1
+    row = next(len(i) for i in dic.values())
+    col = next(i.shape[1] for i in dic.values())
     for i in range(0, row * col + 1):
         print(dic.keys())
 
