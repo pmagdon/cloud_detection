fe4f22772077fa48b718675d390a4ccb5311c52d
pmagdon <pmagdon@gwdg.de>
2017-10-29 20:21:18 +0100

    Added new function moving_window

--- src/test.py
+++ src/test.py
@@ -73,3 +73,32 @@ def test_3(dic, row, col, size, date):
                     print(row_window, col_window, " is a usable cell")
     return na_matrix
 
+image = np.arange(100).reshape(10,10)
+#image = np.random.randint(0,100,100).reshape(10,10)
+
+def moving_window(array, size,edge='nan'):
+    """ Function to extract the values within an analysis window from an 2d Array
+    Args:
+        array:  the image from which to extract all the moving windows
+        size: the siez of the movong window as odd number
+        edge: a string argument to specify how to handle the edges
+    Returns:
+         None and prints all windows
+    """
+
+    if size%2 == 0:
+        raise ValueError(" Size needs to be odd!")
+    if edge != 'nan':
+        raise ValueError(" Edge argument needs to of 'nan', ...")
+
+    sz = math.floor(size / 2)
+    #Apply padding with nan add edge
+    array = np.pad(array.astype(float),pad_width=sz,mode='constant',constant_values=np.nan)
+
+    for row in range(sz, array.shape[0]-sz):
+        for col in range(sz, image.shape[1]-sz):
+            print(array[row-sz:row + sz + 1, col-sz:col+sz+1])
+
+
+moving_window(image, 3,edge='nan')
+
